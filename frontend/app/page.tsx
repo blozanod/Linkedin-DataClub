80eb9167afd9f1f260ed114cda965e7428f00536
BeegScoop <bfclashman15@gmail.com>
2025-11-19 22:40:36 -0500

    created base frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import axios from "axios";
+
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center p-8">
+      <h1 className="text-5xl">
+        ND DataClub - LinkedIn
+      </h1>
+    </main>
+  );
+}
