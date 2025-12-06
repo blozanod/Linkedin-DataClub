cf0dd9fec6380e8c8926c6bc3b24e24092cb91af
BeegScoop <bfclashman15@gmail.com>
2025-12-06 15:55:11 -0500

    fastapi setup

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,18 @@
-import fastapi
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+
+
+@app.get("/")
+async def root():
+  return {"message": "Hello"}
+
+app.post("/get_jobs")
+async def root():
+  return {"message": "Here are the jobs"}
 
 # Point of communcation between:
 # 1. Helper Functions
